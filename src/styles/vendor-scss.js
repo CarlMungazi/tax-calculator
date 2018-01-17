b7c86c0e3235d4aeaa322c7ffc8a1fdc81acadc5
Carl-Dev <c_mungazi@hotmail.co.uk>
2018-01-17 12:37:40 +0000

    Initial commit using mithril boilerpate

---
+++ src/styles/vendor-scss.js
@@ -0,0 +1,6 @@
+/**** Import all vendor scss here *******/
+// require('font-awesome/scss/font-awesome.scss');
+import 'tachyons-sass/tachyons.scss';
+
+/**** Import app scss *******/
+import './app.scss';
